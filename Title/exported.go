99ff193d83069604d0f6096d435ca948d7ee83ad
Usagi1234 <noinon6@gmail.com>
2024-09-09 18:07:17 +0700

    Exported names

---
+++ Title/exported.go
@@ -0,0 +1,11 @@
+package exported
+
+import "fmt"
+
+func Exported() {
+	fmt.Println("Exported names 1")
+}
+
+func Exported2() {
+	fmt.Println("Exported names 2")
+}
--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 
-	"github.com/Usagi1234/go_basic101/hello"
+	exported "github.com/Usagi1234/go_basic101/Title"
 )
 
 func main() {
+
+	//* Exported names
 	fmt.Println("basic101")
-	hello.Hello()
-	// hello.Hello2()
+	exported.Exported() // เรื่องการทำ Exported names หรือก็คือการดึง package มาใช้งาน
+	//*การสร้างตัวแปรขึ้นต้นด้วยตัวพิมใหญ่จะสามารถส่งออกมาใช้งานได้
+
+	// exported.Exported2()
 }
